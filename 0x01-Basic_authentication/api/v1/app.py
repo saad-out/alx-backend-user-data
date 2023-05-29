aa3a80ac6c6fd72377929b022433b04c82c176ff
saad-out <outsaad03@gmail.com>
2023-05-29 17:10:06 +0100

    Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,27 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if getenv("AUTH_TYPE", "") == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def filter_request():
+    """ Filter incoming request
+    """
+    if auth is None:
+        return
+
+    excluded = ['/api/v1/status/',
+                '/api/v1/unauthorized/',
+                '/api/v1/forbidden/']
+    if auth.require_auth(request.path, excluded):
+        if not auth.authorization_header(request):
+            abort(401)
+        elif not auth.current_user(request):
+            abort(403)
 
 
 @app.errorhandler(404)
