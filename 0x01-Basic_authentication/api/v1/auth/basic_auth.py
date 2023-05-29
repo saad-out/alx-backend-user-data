3e8186c57505c64bb408f881b326418030ce99c0
saad-out <outsaad03@gmail.com>
2023-05-29 19:53:01 +0100

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,18 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Class to manage the app authentication.
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str
+                                            ) -> str:
+        AUTH_SCHEME = "Basic "
+
+        if authorization_header is None:
+            return None
+        try:
+            assert type(authorization_header) == str
+        except AssertionError:
+            return None
+        if not authorization_header.startswith(AUTH_SCHEME):
+            return None
+
+        return authorization_header[len(AUTH_SCHEME):]
