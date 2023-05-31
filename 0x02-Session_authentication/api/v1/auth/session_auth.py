9a707e74d0bfc699e8bba9165901ae56cd039d5b
saad-out <outsaad03@gmail.com>
2023-05-31 22:58:01 +0100

    Empty SessionAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module of class SessionAuth to manage the API authentication.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Class to manage the app authentication.
+    """
+    pass
