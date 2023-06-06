3deba4a8290294a7c2130ddb7fb3e45481e8bc0d
saad-out <outsaad03@gmail.com>
2023-06-06 16:18:40 +0100

    create session for user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -49,3 +49,14 @@ class Auth:
                                   user.hashed_password.encode("utf8"))
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Create session ID for user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        session_id = _generate_uuid()
+        self._db.update_user(user_id=user.id, session_id=session_id)
+        return session_id
