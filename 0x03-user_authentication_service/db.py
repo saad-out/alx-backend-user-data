9f2666f0e3f8cf75b5bd278e4ed7fcc5d4fafee3
saad-out <outsaad03@gmail.com>
2023-06-06 11:17:30 +0100

    change type annotation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
 
 from user import Base, User
 
@@ -30,10 +32,27 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
         """Add User to database
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, *args, **kwargs) -> User:
+        """Returns first User row as per kwargs filters
+        """
+        user_query = self._session.query(User)
+        filter_conditions = []
+        for column, value in kwargs.items():
+            filter_conditions.append(getattr(User, column) == value)
+        print()
+        print(filter_conditions)
+        print()
+        for colum, value in kwargs.items():
+            user_query = user_query.filter_by(colum=value)
+        filtered_user = user_query.first()
+        if filtered_user is None:
+            raise NoResultFound
+        return filtered_user
