68dc9dff5878b2cbf823ce9c7aa7db4771d7c164
saad-out <outsaad03@gmail.com>
2023-06-07 12:11:31 +0100

    udpate password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,3 +88,14 @@ class Auth:
         reset_token = str(uuid.uuid4())
         self._db.update_user(user_id=user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update user password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        hashed_password = _hash_password(password).decode("utf-8")
+        self._db.update_user(user_id=user.id, hashed_password=hashed_password)
+        return None
