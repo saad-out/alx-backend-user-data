c32f72f32a4778afc864c604e743dd561548e038
saad-out <outsaad03@gmail.com>
2023-06-07 12:27:24 +0100

    update password endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,5 +82,19 @@ def get_reset_password_token() -> str:
     return jsonify({"email": email, "reset_token": reset_token})
 
 
+@app.route("/reset_password", methods=["PUT"])
+def update_password() -> str:
+    """Update user password
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        return abort(403)
+    return jsonify({"email": email, "message": "Password updated"})
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
