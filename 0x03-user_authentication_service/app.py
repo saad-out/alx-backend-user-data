125a3e3e4abac12d4662ddd2f1cb4415f33ccc4b
saad-out <outsaad03@gmail.com>
2023-06-07 11:36:50 +0100

    log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic Flask app
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 
 from auth import Auth
 
@@ -41,6 +41,20 @@ def login() -> str:
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
     return response
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout() -> str:
+    """Logout user
+    """
+    session_id = request.cookies.get("session_id")
+    if session_id is None:
+        return abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        return abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == '__main__':
